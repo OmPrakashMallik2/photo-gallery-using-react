56d3beecbcbdc66f841b5d7181919e8a71b8e425
Om Prakash Mallik <opm202000@gmail.com>
2024-02-08 12:29:18 +0530

    building Photo gallery app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-5xl text-blue-600'>Photo Gallery</h1>
+    </div>
+  );
+}
+
+export default App;
